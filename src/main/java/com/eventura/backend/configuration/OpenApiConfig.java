db210bd7ae4fd16bf7f3724970cf6ecd3d435213
Jules CROIZIER <99172254+roulios44@users.noreply.github.com>
2025-03-13 11:01:37 +0100

    feature:updating swagger configuration to display only needed routes (#19) (#20)

---
+++ src/main/java/com/eventura/backend/configuration/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package com.eventura.backend.configuration;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public GroupedOpenApi customApi() {
+        return GroupedOpenApi.builder()
+                .group("custom-api") // Nom du groupe
+                .pathsToMatch("/api/**") // N'expose que les routes commençant par /api
+                .build();
+    }
+}
